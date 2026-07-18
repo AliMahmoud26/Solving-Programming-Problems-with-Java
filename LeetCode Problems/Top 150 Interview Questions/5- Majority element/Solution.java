0530979010e05acd7827c6053acc222d9060d3d8
AliMahmoud26 <alimah.engineer@gmail.com>
2026-07-18 15:02:06 +0300

    Publishing the primary solution for the majority element question on LeetCode

---
+++ LeetCode Problems/Top 150 Interview Questions/5- Majority element/Solution.java
@@ -0,0 +1,36 @@
+public class Solution {
+    public int majorityElement(int[] nums) {
+        int candidate = 0, count = 0;
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            } else {
+                count += (num == candidate) ? 1 : -1;
+            }
+        }
+        return candidate;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        
+        // Test Case 1
+        int[] nums1 = {3, 2, 3};
+        System.out.println("Majority Element: " + solution.majorityElement(nums1)); // Expected: 3
+        
+        // Test Case 2
+        int[] nums2 = {2, 2, 1, 1, 1, 2, 2};
+        System.out.println("Majority Element: " + solution.majorityElement(nums2)); // Expected: 2
+        
+        // Test Case 3
+        int[] nums3 = {1};
+        System.out.println("Majority Element: " + solution.majorityElement(nums3)); // Expected: 1
+        
+        // Test Case 4
+        int[] nums4 = {6, 5, 5};
+        System.out.println("Majority Element: " + solution.majorityElement(nums4)); // Expected: 5
+
+        // Test Case 5
+        int[] nums5 = {3, 3, 4};
+        System.out.println("Majority Element: " + solution.majorityElement(nums5)); // Expected: 3
+}}
