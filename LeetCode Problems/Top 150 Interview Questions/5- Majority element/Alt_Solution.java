86eb699e93220d3dbd30da7884550dd64c01bef8
AliMahmoud26 <alimah.engineer@gmail.com>
2026-07-18 15:03:23 +0300

    Publishing the secondry/alternative solution for the majority element question on LeetCode

---
+++ LeetCode Problems/Top 150 Interview Questions/5- Majority element/Alt_Solution.java
@@ -0,0 +1,29 @@
+public class Alt_Solution {
+    public int majorityElement(int[] nums) {
+        int candidate = 0;
+    int count = 0;
+    
+    for (int num : nums) {
+        if (count == 0) {
+            candidate = num;
+            count = 1;
+        } else if (num == candidate) {
+            count++;
+        } else {
+            count--;
+        }
+    }
+    return candidate;
+    }
+
+    public static void main(String[] args) {
+        Alt_Solution solution = new Alt_Solution();
+
+        // Test Case 1
+        int[] nums1 = {6, 5, 5};
+        System.out.println("Majority Element: " + solution.majorityElement(nums1)); // Expected: 5
+
+        // Test Case 2
+        int[] nums2 = {3, 3, 4};
+        System.out.println("Majority Element: " + solution.majorityElement(nums2)); // Expected: 3
+}}
