3a3908fe755d16c531a843f1cd8afcbfd7012dfc
AliMahmoud26 <alimah.engineer@gmail.com>
2026-07-20 15:05:35 +0300

    Publishing the solution for the rotate array question

--- LeetCode Problems/Top 150 Interview Questions/6- Rotate Array/Solution.java
+++ LeetCode Problems/Top 150 Interview Questions/6- Rotate Array/Solution.java
@@ -8,8 +8,32 @@
 // rotate 2 steps to the right: [6,7,1,2,3,4,5]
 // rotate 3 steps to the right: [5,6,7,1,2,3,4]
 
+import java.util.Arrays;
+
 public class Solution {
-    public static void main(String args[]) {
-        System.out.println("Hello World");
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        k = k % n; // Handle cases where k is greater than n
+        reverse(nums, 0, n - 1); // Reverse the entire array
+        reverse(nums, 0, k - 1); // Reverse the first k elements
+        reverse(nums, k, n - 1); // Reverse the remaining elements
+    }
+
+    private void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = {1, 2, 3, 4, 5, 6, 7};
+        int k = 3;
+        solution.rotate(nums, k);
+        System.out.println(Arrays.toString(nums)); // Output: [5, 6, 7, 1, 2, 3, 4]
     }
 }
